1b896e0cb9f3ccd811b7c89afca456ee5de45adf
captainklesel <calum.klesel@gmail.com>
2024-01-13 04:10:50 -0700

    removed export from config

--- mymobilervtechsitev2/next.config.js
+++ mymobilervtechsitev2/next.config.js
@@ -5,8 +5,6 @@
 const path = require('path')
 
 const nextConfig = {
-    output: 'export',
-   
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
    
